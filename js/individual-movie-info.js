12aa492174376c748d757b324020defc016a6793
antoniooooooooooooooooo <apedrosimoes39@gmail.com>
2024-01-05 20:47:38 +0000

    update photos individual

--- js/individual-movie-info.js
+++ js/individual-movie-info.js
@@ -4,8 +4,8 @@ const movieSlug = urlParams.get('slug');
 console.log('Movie Slug:', movieSlug);
 
 // Fetch data from the API using the extracted slug
+// Fetch data from the API using the extracted slug
 const url = `https://api.cosmicjs.com/v3/buckets/fita-production/objects/${movieSlug}?read_key=nMpklsOUy4PFd7cy1DtpXwvKDAst2IXyCGC4I4x2cDynYnbkUF&depth=1&props=slug,title,metadata`;
-console.log(url);
 
 fetch(url)
     .then(response => {
@@ -33,9 +33,11 @@ fetch(url)
             document.getElementById('movie-details-realizacao').textContent = movieData.realizacao;
             document.getElementById('movie-details-descricao').textContent = movieData.descricao;
             document.getElementById('movie-details-plataformas').textContent = movieData.plataformas;
+            
+            // Set photo2 and photo3 to the same value as photo1 if they are not present
             document.getElementById('movie-details-photo1').src = movieData.photo1.url;
-            document.getElementById('movie-details-photo2').src = movieData.photo2.url;
-            document.getElementById('movie-details-photo3').src = movieData.photo3.url;
+            document.getElementById('movie-details-photo2').src = movieData.photo2 ? movieData.photo2.url : movieData.photo1.url;
+            document.getElementById('movie-details-photo3').src = movieData.photo3 ? movieData.photo3.url : movieData.photo1.url;
         } else {
             throw new Error('Invalid API response structure or movie not found');
         }
