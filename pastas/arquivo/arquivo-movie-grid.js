0dec4537497417323a233af307c4f8b29a3bbc2c
antoniooooooooooooooooo <apedrosimoes39@gmail.com>
2024-01-09 18:36:42 +0000

    9

--- pastas/arquivo/arquivo-movie-grid.js
+++ pastas/arquivo/arquivo-movie-grid.js
@@ -241,7 +241,7 @@ function fetchMovies(apiUrl, sortingOption) {
 
                 // Create a link to the movie details page
                 const movieLink = document.createElement('a');
-                movieLink.href = ``;
+                movieLink.href = `pastas/arquivo/index.html?slug=${movie.id}`;
  // Pass the movie slug as a query parameter
                 movieLink.innerHTML = `<img src="${movie.metadata.photo.url}" alt="${movie.title}">`;
 
